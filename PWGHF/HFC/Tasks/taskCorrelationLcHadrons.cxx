4aa82ce7dc1d7409a1c247104c7736a97ac712d5
Tushar Bhalla <bht7899@gmail.com>
2025-07-21 19:16:06 +0530

    megalinter error fix 2

--- PWGHF/HFC/Tasks/taskCorrelationLcHadrons.cxx
+++ PWGHF/HFC/Tasks/taskCorrelationLcHadrons.cxx
@@ -14,7 +14,6 @@
 /// \author Marianna Mazzilli <marianna.mazzilli@cern.ch>
 /// \author Zhen Zhang <zhenz@cern.ch>
 
-
 #include "PWGHF/Core/DecayChannels.h"
 #include "PWGHF/Core/HfHelper.h"
 #include "PWGHF/Core/SelectorCuts.h"
@@ -55,11 +54,6 @@
 #include <cstddef>
 #include <cstdint>
 #include <cstdlib>
-#include "CCDB/BasicCCDBManager.h"
-#include "Framework/AnalysisTask.h"
-#include "Framework/HistogramRegistry.h"
-#include "Framework/runDataProcessing.h"
-
 #include <memory> // std::shared_ptr
 #include <string>
 #include <vector>
